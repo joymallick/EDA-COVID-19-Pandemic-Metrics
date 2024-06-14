f8d91c82c1d2d485815b1ad578ed1b690c613ba9
margheritacrn <119339125+margheritacrn@users.noreply.github.com>
2024-06-14 12:31:44 +0200

    Created python scripts for processing steps

---
+++ bin/dataprocessing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+The file processes the input dataset (csv format)
+"""
+import pandas as pd
+import os
+
+
+def process_csvfile(filename: str, drop_na=False, impute_na=False):
+    pass
+
+
+def collapse_by_time_period(data: pd.DataFrame, time_period: str, method: str):
+    pass
---
+++ bin/outcomes_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+The file processes the input dataset (csv format)
+"""
+import pandas as pd
+import os
+import numpy as np
+
+
+def normalize_by():
+    pass
+
+
+def percentage_growth():
+    pass
