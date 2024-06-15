16ac9bee32f7bd3e1ae86847c9031cc6ecb40868
Nick <nick.thomas@uni-potsdam.de>
2024-06-15 18:29:27 +0200

    added normalization function

--- bin/outcomes_utils.py
+++ bin/outcomes_utils.py
@@ -7,9 +7,16 @@ import os
 import numpy as np
 
 
-def normalize_by():
-    pass
+def normalize_column(col: pd.Series, norm: pd.Series)-> pd.DataFrame:
+    """The function creates a normalized version of a dataset column.
+    The missing values are assumed to be already handeled.
 
+        Args:
+            col (pd.Series): column to normalize
+            norm (pd.Series): column used for normalization
+    """
+    normalized_col = col/norm
+    return normalized_col
 
 def percentage_growth():
     pass
