06715080e472294c9c4807b4bf4830ac1365f202
margheritacrn <119339125+margheritacrn@users.noreply.github.com>
2024-06-18 19:34:29 +0200

    fixed error in processing function

--- bin/dataprocessing.py
+++ bin/dataprocessing.py
@@ -12,7 +12,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 BIN_PATH = os.getcwd()
-TIME_COLUMNS = ['semester', 'month', 'year', 'date']
 
 
 def convert_to_datetime(date, format='%Y-%m-%d'):
@@ -55,40 +54,12 @@ def process_csvfile(filename: str) -> pd.DataFrame:
     # convert date to datetime 
     df.date = df.date.apply(convert_to_datetime)   
     # create uniquely identified month , year and semester columns
+    logging.debug('adding new time columns')
     df['year'] = df.date.apply(get_years)
     df['month'] = df.date.dt.to_period('M')
     df['semester'] = df.date.apply(get_semester)
     return df
 
-
-def collapse_by_time_period(df: pd.DataFrame, time_period='semester', aggr='mean')->pd.DataFrame:
-    """"The function creates a collapsed version of the input dataset on
-    either years or semesters by taking sum/mean of the values or last value.
-    The input dataset must contain only numeric columns. 
-
-    Args:
-        df (pd.DataFrame): dataframe to collapse
-        time_period (str): either 'semester' or 'year'
-        aggr (str): aggregation function among 'mean','sum','last'
-    """
-    # checks on Args:
-    if (df is None):
-        message = "Provide a pandas dataframe"
-        raise ValueError(message)
-    if (time_period not in ['semester','year','month']):
-        message = "The time period can be either 'year', 'month' or 'semester'"
-    if (aggr not in ['mean','sum','last']):
-        message = "The aggregation function must be one among 'mean','sum' and 'last'"
-        raise ValueError(message)
-    logging.debug('dropping time columns other than time_period')
-    time_cols_to_drop = [tc for tc in TIME_COLUMNS if tc != time_period]
-    df = df.drop(columns=time_cols_to_drop)
-    logging.debug('identifiying non-numerical columns and adding time_period')
-    non_num_cols = list(df.select_dtypes(exclude=['number']).columns)
-    non_num_cols.append(time_period)
-    logging.debug('collapsing by specified time period and aggr function')
-    collapsed_df = df.groupby(non_num_cols).agg(aggr)
-    return collapsed_df
 
 
 def main(csvfile: str)->pd.DataFrame:
@@ -108,7 +79,7 @@ if __name__ == "__main__":
     aggr_funs = ['mean', 'sum', 'last']
     parser = argparse.ArgumentParser(
         description='The file  applies initial processing steps to the csv file')
-    parser.add_argument('csvfile', type=str, help='Csv file name', required=True)
+    parser.add_argument('csvfile', type=str, help='Csv file name')
     args = parser.parse_args()
     main(args.csvfile)
   
