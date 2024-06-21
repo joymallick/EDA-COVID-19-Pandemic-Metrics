ff52b6b1042a468f7bf19f379e07d78e412576ec
margheritacrn <119339125+margheritacrn@users.noreply.github.com>
2024-06-21 12:23:01 +0200

    Added output file as CL input and fixed plot code

--- bin/regressionplot_rq3.py
+++ bin/regressionplot_rq3.py
@@ -5,6 +5,10 @@ The script produces scatter and regression line plots for RQ 3.
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+import os
+import pandas as pd
+import argparse
 
 
 logger = logging.getLogger(__name__)
@@ -15,7 +19,7 @@ SNS_CUSTOM_CONFIG_PARAMS = {"axes.spines.right": False,
 sns.set_theme(palette='pastel', style='ticks', rc=SNS_CUSTOM_CONFIG_PARAMS)
 
 
-def lm_plot(x: str, y: str, data: pd.DataFrame, xlabel: str, ylabel: str, title:str):
+def reg_plot(x: str, y: str, data: pd.DataFrame, xlabel: str, ylabel: str, title:str):
     """The function produces a regression+scatter plot
     using seaborn for x and y columns of data.
     Args:
@@ -31,26 +35,32 @@ def lm_plot(x: str, y: str, data: pd.DataFrame, xlabel: str, ylabel: str, title:
         figure (matplotlib.figure.Figure)
     """
     fig, ax = plt.subplots()
-    sns.lmplot(x=x, y=y, data=data, fit_reg=True, ax=ax)
+    sns.regplot(x=x, y=y, data=data, fit_reg=True, ax=ax)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title(title)
-    figure.tight_layout()
+    fig.tight_layout()
     return fig
 
 
-def main(processedcsvfile_rq3: str):
-    data_rq3 = pd.read_cs(processedcsvfile)
+def main(processedcsvfile_rq3: str, outpngfile: str):
+    os.chdir(r"..\data")
+    data_rq3 = pd.read_csv(processedcsvfile_rq3)
+    os.chdir(BIN_PATH)
+    if('germany' in processedcsvfile_rq3):
+        geolevel = 'Germany'
+    else:
+        geolevel ='Europe'
     logging.basicConfig(filename='trendplots_rq3.log')
     logger.info('Started producing reg plot')
     # plot
     logging.debug('Getting reg plot')
-    fig = lm_plot(x='new_vaccinations', y='deaths_vs_cases', data=data_rq3,
+    fig = reg_plot(x='new_vaccinations', y='deaths_vs_cases', data=data_rq3,
                   xlabel='new vaccinations', ylabel='deaths/cases',
-                  title='OLS for new vaccinations and deaths over cases- Europe (by month)')
+                  title=f'OLS for new vaccinations and deaths over cases- {geolevel} (by month)')
     os.chdir(r"..\results\rq3\plots")
     logger.info('Saving plots')
-    # add saving figure 
+    fig.savefig(outpngfile[:-4]+f"_{geolevel}.png")
     os.chdir(BIN_PATH)
     logger.info('Ended producing reg plot')
 
@@ -59,5 +69,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='The file produces a regression plot for RQ 3')
     parser.add_argument('processedcsvfile_rq3', type=str, help='csvfile processed for RQ 3')
+    parser.add_argument('outpngfile', type=str, help='output png file to save the plot')
     args = parser.parse_args()
-    main(args.processedcsvfile_rq3)
+    main(args.processedcsvfile_rq3, args.outpngfile)
