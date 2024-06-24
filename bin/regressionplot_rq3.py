30bea293265ed881773e7b4483e0689ac4bff4eb
margheritacrn <119339125+margheritacrn@users.noreply.github.com>
2024-06-24 10:23:34 +0200

    refactored code in main method

--- bin/regressionplot_rq3.py
+++ bin/regressionplot_rq3.py
@@ -10,12 +10,13 @@ import argparse
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 SNS_CUSTOM_CONFIG_PARAMS = {"axes.spines.right": False,
                            "axes.spines.top": False}
 sns.set_theme(palette='pastel', style='ticks', rc=SNS_CUSTOM_CONFIG_PARAMS)
 
 
-def reg_plot(x: str, y: str, data: pd.DataFrame, xlabel: str, ylabel: str, title:str):
+def reg_plot(x, y, data, xlabel, ylabel, title):
     """The function produces a regression+scatter plot
     using seaborn for x and y columns of data.
     Args:
@@ -40,12 +41,20 @@ def reg_plot(x: str, y: str, data: pd.DataFrame, xlabel: str, ylabel: str, title
 
 
 def main(processedcsvfile_rq3: str, outpngfile: str):
+    if (processedcsvfile_rq3[-3:] != 'csv'):
+        message = "Provide a csv file as infile"
+        logger.exception(message)
+        raise OSError(message)
+    if (outpngfile[-3:] != 'png'):
+        message = "Provide a png file as outfile"
+        logger.exception(message)
+        raise OSError(message)
     data_rq3 = pd.read_csv(processedcsvfile_rq3)
     if('germany' in processedcsvfile_rq3):
         geolevel = 'Germany'
     else:
         geolevel ='Europe'
-    logging.basicConfig(filename='trendplots_rq3.log')
+    logging.basicConfig(filename='regressionplot_rq3.log')
     logger.info('Started producing reg plot')
     fig = reg_plot(x='new_vaccinations', y='deaths_vs_cases', data=data_rq3,
                   xlabel='new vaccinations', ylabel='deaths/cases',
