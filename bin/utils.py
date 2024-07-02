ed1c439f4800e7e36490f31e9fc0ea5a14488bfd
margheritacrn <119339125+margheritacrn@users.noreply.github.com>
2024-07-02 18:34:52 +0200

    added function to set configurable plot params

--- bin/utils.py
+++ bin/utils.py
@@ -1,5 +1,5 @@
 import yaml
-
+import matplotlib.pyplot as plt
 
 def load_config(config_file):
     """The function loads configuration parameters
@@ -21,3 +21,20 @@ def load_config(config_file):
         config = yaml.safe_load(file)
 
     return config
+
+
+def set_plot_params(config_file):
+    """The function sets the matplotlib parameters
+    according to the ones provided in config_file.
+
+    Args:
+        config_file (_type_): path of the configuration file
+    Returns:
+        None
+    """
+    plot_params = load_config(config_file)
+    for param, value in plot_params.items():
+        if (param == 'style'):
+            plt.style.use(value)
+        else:
+            plt.rcParams[param] = value
