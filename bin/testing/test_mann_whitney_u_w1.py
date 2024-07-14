0082ffa2a6dee03d359606cc12e69e5c20c2873e
margheritacrn <119339125+margheritacrn@users.noreply.github.com>
2024-07-14 16:47:55 +0200

    finished test for man whitney u

--- bin/testing/test_mann_whitney_u_w1.py
+++ bin/testing/test_mann_whitney_u_w1.py
@@ -1,61 +1,37 @@
 #!/usr/bin/env python3
 """
-The script contains a unit test for the component bin/dataprocessing_w3.py
-and an integration test for the components bin/dataprocessing.py
-and bin/dataprocessing_w3.py.
+The script contains a unit test for the component
+bin/workflow_1/mann_whitney_u_w1.py.
 """
-from bin.workflow_1.mann_whitney_u import mann_whitney_u_test
+from bin.workflow_1.mann_whitney_u_w1 import mann_whitney_u_test
+import os
+import csv
 
 
 def mock_csv_data():
     """Fixture to create a mock CSV file."""
-    data = """x_variable,y_variable
-10,13
-12,27
-8,9
-1,31
-4,47
-"""
-    with open("../..data/mann_whitney_u_testing_data.csv", "w") as f:
-        f.write(data)
-    yield "../..data/mann_whitney_u_testing_data.csv"
-    # Cleanup after test
-    os.remove("../..data/mann_whitney_u_testing_data.csv")
+    data = [['x_variable', 'y_variable'],
+            [1, 13],
+            [0, 27],
+            [1, 9],
+            [1, 31],
+            [0, 47]]
+    filename = "../../data/mann_whitney_u_testing_data.csv"
 
-def run_test(cat_column):
+    with open(filename, "w") as f:
+        writer = csv.writer(f)
+        # f.write(data)
+        writer.writerows(data)
+    return filename
+
+
+def test_mann_whitney_u_test():
     """Helper function to run the test with the given parameters."""
-    filename = "../../data/owid-covid-data_processed.csv"
-    test_df = create_df()
-    expected_u, expected_p = [2, 0.3662]
-    actual_u, actual_p = mann_whitney_u_test("../..data/mann_whitney_u_testing_data.csv", x_variable="x_variable", y_variable="y_variable", output="testing.txt")
-
+    filename = mock_csv_data()
+    expected_u, expected_p = [1, 0.4]
+    actual_u, actual_p = mann_whitney_u_test(filename, x_variable="x_variable",
+                                             y_variable="y_variable",
+                                             output="testing.txt", logger=True)
     assert expected_u == actual_u
     assert expected_p == actual_p
-
-
-
-def test_process_csvfile_w1_gdp_per_capita():
-    """Unit test for function process_csvfile_w1 with gdp_per_capita."""
-    run_test('gdp_per_capita')
-
-
-def test_process_csvfile_w1_life_expectancy():
-    """Unit test for function process_csvfile_w1 with life_expectancy."""
-    run_test('life_expectancy')
-
-
-def test_process_csvfile_w1_median_age():
-    """Unit test for function process_csvfile_w1 with median_age."""
-    run_test('median_age')
-
-
-def test_process_csvfile_w1_population_density():
-    """Unit test for function process_csvfile_w1 with population_density."""
-    run_test('population_density')
-
-
-if __name__ == "__main__":
-    test_process_csvfile_w1_gdp_per_capita()
-    test_process_csvfile_w1_life_expectancy()
-    test_process_csvfile_w1_median_age()
-    test_process_csvfile_w1_population_density()
+    os.remove(filename)
